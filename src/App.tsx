fd4cc9dbb188e59edd7871a92c42fa5638850720
Trahaswi <102136210+Trahaswi@users.noreply.github.com>
2022-06-12 15:45:20 +0530

    Update App.tsx
    edited some stuff heh

--- src/App.tsx
+++ src/App.tsx
@@ -57,15 +57,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Welcome to swittor</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>You have been found you sussy</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Whoeve sees this is sus<IonLabel/>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
